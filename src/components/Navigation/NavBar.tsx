6bfa33869aa3fb3fb353213e17a805087d62ef78
philcon93 <phil@neto.com.au>
2021-06-24 08:27:23 +1000

    added NavBar

--- src/components/Navigation/NavBar.tsx
+++ src/components/Navigation/NavBar.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useHistory } from 'react-router';
+import { TopBar } from '@shopify/polaris';
 import constants from '../../store/constants';
 
 export const NavBar: React.FC = () => {
     const history = useHistory();
 
+    const handleNavigationToggle = useCallback(() => {
+        history.push(constants.DASHBOARD_ROUTE)
+      }, []);
+
     return (
-        <div>
-            <a onClick={() => history.push(constants.DASHBOARD_ROUTE)}>Home</a>
-        </div>
+        <TopBar
+            showNavigationToggle
+            onNavigationToggle={handleNavigationToggle}
+        />
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,20 @@ import {AppProvider} from "@shopify/polaris";
 import en from '@shopify/polaris/locales/en.json';
 import "@shopify/polaris/dist/styles.css";
 import './styles/styles.scss';
+import home from './images/HomeMajor.svg'
+
+const theme = {
+  logo: {
+    width: 30,
+    topBarSource: home,
+    url: '/',
+    accessibilityLabel: 'Goat Surveys'
+  },
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <AppProvider i18n={en}>
+    <AppProvider i18n={en} theme={theme}>
       <App />
     </AppProvider>
   </React.StrictMode>,
